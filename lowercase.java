892900812b48a283f04dd8f2698edbc12a848db9
varun10082004 <101417934+varun10082004@users.noreply.github.com>
2023-02-05 22:37:25 +0530

    Add files via upload

---
+++ lowercase.java
@@ -0,0 +1,6 @@
+public class lowercase{  
+public static void main(String args[]){  
+String s1=" HELLO stRIng";  
+String s1lower=s1.toLowerCase();  
+System.out.println(s1lower);  
+}}  
---
+++ uppercase.java
@@ -0,0 +1,6 @@
+public class uppercase{  
+public static void main(String args[]){  
+String s1="hello string";  
+String s1upper=s1.toUpperCase();  
+System.out.println(s1upper);  
+}}  
